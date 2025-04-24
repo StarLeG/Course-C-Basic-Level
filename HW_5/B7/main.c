40229056eaddd13fa15e53dbd64bf933ec237284
Oleg Starinsky <oleg.starynski@gmail.com>
2025-04-24 18:55:40 +0300

    Add test B7

--- HW_5/B7/main.c
+++ HW_5/B7/main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-// Ввести целое число и определить, верно ли, что в его записи есть две одинаковые цифры, стоящие рядом.
+// Ввести целое число и определить, верно ли, что в его записи есть  две одинаковые цифры, НЕ обязательно стоящие рядом.
 
-int main(int argc, char **argv)
-{
-	
-	return 0;
+int main(){
+    int a;
+    bool flag = false;
+
+    scanf("%d", &a);
+
+    int b;
+
+    while (a != 0)
+    {
+        int tmp1 = a % 10;
+        b = a / 10;       
+
+        while (b != 0)
+        {
+           int tmp2 = b % 10;
+            if (tmp1 == tmp2){
+                flag = true;
+                break;
+            }
+           b /= 10;
+        }
+        
+        a /= 10;
+        b = a;        
+    }
+    
+    flag ? printf("YES\n") : printf("NO\n");
+
+    return 0;
 }
-
