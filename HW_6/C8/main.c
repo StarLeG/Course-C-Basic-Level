f15af6a5dfcf9a367555b94102fb4ef30d2e9c2b
StarLeG <oleg.starynski@gmail.com>
2025-04-27 17:46:26 +0300

    Add Test C8

--- HW_6/C8/main.c
+++ HW_6/C8/main.c
@@ -3,8 +3,25 @@
 // Составить функцию, которая переводит латинскую строчную букву в заглавную.
 // И показать пример ее использования.
 
+char toUpper(char ch);
+
 int main(){
     char c;
 
-    
+    c = getchar();
+
+    while (c != '.')
+    {        
+        putchar(toUpper(c));
+        c = getchar();
+    }
+   
+    return 0;
+}
+
+char toUpper(char ch){
+    if (ch >= 'a' && ch <= 'z') {
+        ch = ch - 'a' + 'A';
+    }
+    return ch;
 }
