1ae0358dcfdf7fdf0f4cfb4f2ee15cdfe8104b16
StarLeG <oleg.starynski@gmail.com>
2025-04-28 10:28:41 +0300

    Update Test A5

--- HW_3/A5/main.c
+++ HW_3/A5/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    int result = scanf("%d%d%d", &a, &b, &c);
+    if(result != 3) abort();
     printf("%.2f", ((double)a + (double)b + (double)c) / 3.);
     return 0;
 }
