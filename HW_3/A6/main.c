921e885772687ad45b2e67fbc98aca6f3f6b9379
StarLeG <oleg.starynski@gmail.com>
2025-04-28 10:30:49 +0300

    Update Test A6

--- HW_3/A6/main.c
+++ HW_3/A6/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int a, b;
-    scanf("%d %d", &a, &b);
+    int result = scanf("%d%d", &a, &b);
+    if(result != 2) abort();
+
     printf("%d\n", a - b);
 
     return 0;
