a8337c0f5593741b8dc133d1e918a1e850e94a34
StarLeG <oleg.starynski@gmail.com>
2025-04-28 10:25:00 +0300

    Update Test A4

--- HW_3/A4/main.c
+++ HW_3/A4/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int a, b, c;
-    scanf("%d %d %d", &a, &b, &c);
+    int result = scanf("%d%d%d", &a, &b, &c);
+    if(result != 3) abort();
     printf("%d+%d+%d=%d\n", a, b, c, a + b + c);
     printf("%d*%d*%d=%d\n", a, b, c, a * b * c);
 
