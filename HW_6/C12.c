b50f99a3cdb976313e0b6eb6a6f8dbf2593689e4
Oleg Starinsky <oleg.starynski@gmail.com>
2025-04-30 13:51:51 +0300

    Add Test C12

--- HW_6/C12.c
+++ HW_6/C12.c
@@ -13,9 +13,7 @@
     Формат результата
     Вещественное число в формате "%.3f"
 */
-
-int power(int n, int p);
-double factorial(int n);
+float myAbc(float x);
 float sinus(float x);
 
 int main(){
@@ -29,31 +27,24 @@ int main(){
     return 0;
 }
 
-int power(int n, int p){
-    int pow = n;
-    for(int i = 1; i < p; i++){
-        pow *= n;
-    }
-    
-    if(p == 0) return 1;
-
-    return pow;
-}
-
-double factorial(int n) {
-
-    int fac = 1;
-
-    for(int i = 1; i <= n; i++){
-        fac *= i;
-    }
-
-    return fac;
-
+float myAbc(float x){
+    return x < 0 ? x *= -1 : x;
 }
 
 float sinus(float x){
     // sin(x) = x - x^3/3! + x^5/5! - x^7/7! + ...(x в радианах)
+    const double PI = 3.14159265358979323846;
 
-    
+    float rad = x * PI / 180;
+    float res = 0.0f;
+    float term = rad;
+    int n = 1;
+
+    while (myAbc(term) >= 0.001) {
+        res += term;
+        term *= -rad * rad / ((2 * n) * (2 * n + 1));
+        n++;
+    }
+
+    return res;
 }
