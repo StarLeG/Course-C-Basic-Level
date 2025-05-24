4afd7b930729bea5d5852f09f96d5d67e78193e5
StarLeG <oleg.starynski@gmail.com>
2025-05-24 07:01:45 +0300

    Add Test E17

---
+++ HW_8/E17.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/*
+                    Только один раз
+    Дан массив из 10 элементов. В массиве найти элементы,
+    которые в нем встречаются только один раз, и вывести их на экран.
+
+    Формат входных данных
+    10 целых чисел через пробел
+
+    Формат результата
+    Элементы которые встречаются только один раз через пробел
+*/
+
+bool initArray(int *array, const int size){
+    int n;
+    for(int i = 0; i < size; i++){
+        if(scanf("%d", &n) != 1) return true;
+        array[i] = n;
+    }
+
+    return false;
+}
+
+void printArray(const int *array, const int size){
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", array[i]);
+    }
+}
+
+void bubleSort(int *array, const int size){ 
+
+    for (int i = 0; i < size - 1; i++) {          // Проходы по массиву
+        for (int j = 0; j < size - i - 1; j++) {  // Сравнение соседних элементов
+            if (array[j] > array[j + 1]) {         // Если текущий элемент больше следующего
+                // Меняем элементы местами
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+bool isUniqueNumb(const int array[], const int size){
+
+}
+
+int main(){
+    #define SIZE 10
+    int array[SIZE] = {0};
+    int arrayFind[SIZE] = {0};
+    int idx = -1;
+
+    if(initArray(array, SIZE)) abort();
+
+    bubleSort(array, SIZE);
+
+    (idx != -1) ? printArray(arrayFind, SIZE) : exit(0);
+
+    return 0;
+}
