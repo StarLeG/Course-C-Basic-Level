759503316e1addf2943db7abe3012b507282d948
Oleg Starinsky <oleg.starynski@gmail.com>
2025-05-24 15:24:21 +0300

    Add Test E19

---
+++ HW_8/E19.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+/*
+            Цифры по порядку
+    Вывести в порядке следования цифры,
+    входящие в десятичную запись натурального числа N.
+
+    Формат входных данных
+    Одно натуральное число N
+
+    Формат результата
+    Цифры через пробел
+*/
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+int countDigit(int num){
+    if(num == 0) return 1;
+
+    int cnt = 0;
+
+    while (num != 0)
+    {
+        num /= 10;
+        cnt++;
+    }
+    return cnt;
+}
+
+void printArray(const int* array){    
+
+    for(int i = 0; i < ARRAY_SIZE(array); i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+void splitNumberIntoDigits(int number, int array[]){
+    if(number == 0){
+        array[0] = 0;
+        return;
+    }
+
+    int idx = 0;
+
+    while (number > 0)
+    {
+        array[idx] = number % 10;
+        number /= 10;
+        idx++;
+    }
+    
+}
+
+int main(){
+    int number = 0;
+    
+    if(scanf("%d", &number) != 1 || number < 0) abort();
+     
+
+    int array[number];
+
+    splitNumberIntoDigits(number, array);
+    
+
+    printArray(array);
+
+
+}
