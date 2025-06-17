a55fcbafb8f2176eec31890f234551b464090874
Oleg Starinsky <oleg.starynski@gmail.com>
2025-06-17 18:46:57 +0300

    Update Test G19

--- HW_10/G19.c
+++ HW_10/G19.c
@@ -20,6 +20,14 @@
 
 #define LEN 1001
 
+void countChars(const char *buffer,int freq[], const int len){
+      for (int i = 0; i < len; i++) {
+        if (isalpha(buffer[i])) {
+            freq[buffer[i] - 'a']++;
+        }
+    }
+}
+
 int main(){
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
@@ -40,6 +48,48 @@ int main(){
     }
 
     buffer[strcspn(buffer, "\n")] = '\0';
+
+    int freq[26] = {0};
+    int len = strlen(buffer);    
+
+    countChars(buffer, freq, len);
+
+    char left[LEN] = {0}; 
+    char middle = '\0';    
+    int left_pos = 0;
+
+    for (int i = 0; i < 26; i++) {
+        if (freq[i] == 0) continue;
+        
+        int count = freq[i] / 2;
+        for (int j = 0; j < count; j++) {
+            left[left_pos++] = 'a' + i;
+        }
+
+        
+        if (freq[i] % 2 != 0) {            
+            if (middle == '\0' || ('a' + i) < middle) {
+                middle = 'a' + i;
+            }
+        }
+    }
+
+    char right[LEN] = {0};
+    for (int i = 0; i < left_pos; i++) {
+        right[i] = left[left_pos - 1 - i];
+    }
+    right[left_pos] = '\0';
+
+    char palindrome[LEN * 2] = {0};
+    strcpy(palindrome, left);
+    if (middle != '\0') {
+        palindrome[left_pos] = middle;
+        strcat(palindrome, right);
+    } else {
+        strcat(palindrome, right);
+    }
+
+    fprintf(output, "%s", palindrome);
 
     fclose(input);
     fclose(output);
