0eaf7ef83cd8d2f83de6f2de20f23a77c8d8fa32
Oleg Starinsky <oleg.starynski@gmail.com>
2025-06-17 13:57:00 +0300

    Test data added

--- FinalWork/main.c
+++ FinalWork/main.c
@@ -3,5 +3,17 @@
 
 int main(){
 
+    addRecord(2023, 1, 15, 12, 30, -5);
+    addRecord(2023, 1, 15, 18, 45, -7);
+    addRecord(2023, 2, 20, 10, 15, 10);
+    addRecord(2023, 2, 21, 9, 0, 8);
+    addRecord(2023, 12, 31, 23, 59, -15);
+
+    printf("Monthly Statistics for 2023:\n");
+    printMonthlyStats(2023);
+
+    printf("\nYearly Statistics for 2023:\n");
+    printYearlyStats(2023);
+
     return 0;
 }
