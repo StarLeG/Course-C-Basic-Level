8aac5c3eb8fb405091df73a0f9048a04da842c83
StarLeG <oleg.starynski@gmail.com>
2025-06-21 22:09:15 +0300

    Обновил Run.bat

--- FinalWork/Run.bat
+++ FinalWork/Run.bat
@@ -1,2 +1,5 @@
 cd FinalWork
-Temperature_Statistics.exe 
+:: Temperature_Statistics.exe -h
+:: Temperature_Statistics.exe -m 2
+:: Temperature_Statistics.exe -f "temperature_small.csv"
+ Temperature_Statistics.exe -f "temperature_small.csv" -m 2
